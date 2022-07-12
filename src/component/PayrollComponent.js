c51543b1cdf13b45bb35a8c468a5afecae7a57b7
Hải Nguyễn <nguyendjnhhaiqw@gmail.com>
2022-07-13 00:07:02 +0700

    upload code to github

--- src/component/PayrollComponent.js
+++ src/component/PayrollComponent.js
@@ -5,15 +5,20 @@ import { Media } from "reactstrap";
 
  export function Payroll(props){
     const staffs = props.staffs.map((staffs) =>{
+        const { name, id, salaryScale, overTime } = staffs;
+        const salaryResult = new Intl.NumberFormat("vi-VN", { style: "currency", currency: "VND" }).format(
+          salaryScale * 3000000 + overTime * 200000
+        );
+    
         return(
             <div className="col l-3 m-5 c-12">
                 <div className="card">
-                    <h3>{staffs.name}</h3>
-                    <p>Mã nhân viên: {staffs.id}</p>
-                    <p>Hệ số lương: {staffs.salaryScale}</p>
-                    <p>Số ngày làm thêm: {staffs.overTime}</p>
+                    <h3>{name}</h3>
+                    <p>Mã nhân viên: {id}</p>
+                    <p>Hệ số lương: {salaryScale}</p>
+                    <p>Số ngày làm thêm: {overTime}</p>
                     <div className='role'>
-                    {/* <p>Lương: salaryScale * 3000000 + overTime * 200000</p> */}
+                        <p>lương: {salaryResult}</p>
                     </div>
                 </div>
             </div>
